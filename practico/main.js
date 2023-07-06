f5a3eb2f62a6ff8d5d7feffe80b119014d9ce77c
rpatino12 <rpatino12@outlook.com>
2023-07-06 17:34:08 -0500

    Fusion del navbar con el mobile-menu

--- practico/main.js
+++ practico/main.js
@@ -1,8 +1,17 @@
 const desktopMenu = document.querySelector('.desktop-menu');
 const userEmailButton = document.querySelector('.navbar-email p');
 
+const mobileMenu = document.querySelector('.mobile-menu');
+const burgerMenuButton = document.querySelector('nav .menu');
+
 userEmailButton.addEventListener('click', toggleDesktopMenu);
+
+burgerMenuButton.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
