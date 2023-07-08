9747b726e39123a5423bb103a7ce77bf1c2218e1
rpatino12 <rpatino12@outlook.com>
2023-07-07 19:33:10 -0500

    Integracion del carrito de compras con navbar y otros menus

--- practico/main.js
+++ practico/main.js
@@ -4,14 +4,48 @@ const userEmailButton = document.querySelector('.navbar-email p');
 const mobileMenu = document.querySelector('.mobile-menu');
 const burgerMenuButton = document.querySelector('nav .menu');
 
+const shoppingCartMenu = document.querySelector('.my-order');
+const shoppingCartButton = document.querySelector('.navbar-shopping-cart');
+const shoppingCartCloseButton = document.querySelector('.shopping-cart-close-button');
+
 userEmailButton.addEventListener('click', toggleDesktopMenu);
 
 burgerMenuButton.addEventListener('click', toggleMobileMenu);
 
+shoppingCartButton.addEventListener('click', toggleShoppingCartMenu);
+shoppingCartCloseButton.addEventListener('click', toggleShoppingCartMenu);
+
 function toggleDesktopMenu() {
+    const isShoppingCartMenuOpen = !shoppingCartMenu.classList.contains('inactive');
+
+    if (isShoppingCartMenuOpen) {
+        shoppingCartMenu.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
+    const isShoppingCartMenuOpen = !shoppingCartMenu.classList.contains('inactive');
+
+    if (isShoppingCartMenuOpen) {
+        shoppingCartMenu.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleShoppingCartMenu(){
+    const isMobileMenuOpen = !mobileMenu.classList.contains('inactive');
+    const isDesktopMenuOpen = !desktopMenu.classList.contains('inactive');
+
+    if (isMobileMenuOpen) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    if (isDesktopMenuOpen) {
+        desktopMenu.classList.add('inactive');
+    }
+
+    shoppingCartMenu.classList.toggle('inactive');
 }
